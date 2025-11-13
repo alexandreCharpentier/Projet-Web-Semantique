08b9c52842533a2d68150266fa196589f78d1602
alexandreCharpentier <alex.charpent24@gmail.com>
2025-11-13 20:34:13 +0100

    feat: ajout js

--- js/requete.js
+++ js/requete.js
@@ -1,18 +1,32 @@
 async function fetchData() {
-  const endpoint = "http://localhost:7200/repositories/Projet"; 
+  const endpoint = "http://localhost:3000/s"; 
   const query = `
-    PREFIX ex: <http://cours.iut-orsay.fr/>
-    SELECT * WHERE {
-      ?a ?b ?c
-    } LIMIT 10
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rcw: <https://cours.iut-orsay.fr/rcw/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX com: <https://cours.iut-orsay.fr/commune>
+
+SELECT (?score AS ?scoreVilleFleurie)
+       (ROUND(AVG(xsd:decimal(?no2)) * 10) / 10 AS ?moyenneNO2)
+       (ROUND(AVG(xsd:decimal(?o3)) * 10) / 10 AS ?moyenneO3)
+       (ROUND(AVG(xsd:decimal(?pm10)) * 10) / 10 AS ?moyennePM10)
+	   (ROUND(AVG(xsd:decimal(?densite)) * 10) / 10 AS ?moyenneDensite)
+WHERE {
+    ?com rdf:type rcw:Commune;
+         rcw:scoreFleur ?score;
+         rcw:ville ?ville;
+         rcw:no2 ?no2 ;
+         rcw:o3 ?o3 ;
+         rcw:pm10 ?pm10 ;
+         rcw:population2016 ?population2016 ;
+         rcw:surface ?surface .
+    BIND(xsd:decimal(?population2016) / xsd:decimal(?surface) AS ?densite)
+} 
+GROUP BY ?score
   `;
   
   const url = endpoint + "?query=" + encodeURIComponent(query);
-  const response = await fetch(url, {
-    headers: { "Accept": "application/sparql-results+json" }
-  });
-  
-  const data = await response.json();
-  console.log(data.results.bindings);
+  const data = await fetch("http://localhost:3000/sparql?query=" + encodeURIComponent(query)).then(r => r.json());
+  console.log(data.results_raw);
 }
 fetchData();
