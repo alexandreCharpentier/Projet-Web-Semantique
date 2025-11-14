e86c5ec63cc1fefbdb185632e12154270ef0f741
alexandreCharpentier <alex.charpent24@gmail.com>
2025-11-15 00:46:45 +0100

    feat: up requete

--- js/requete.js
+++ js/requete.js
@@ -1,7 +1,6 @@
 async function fetchData() {
   const endpoint = "http://localhost:3000/s"; 
   const query = `
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX rcw: <https://cours.iut-orsay.fr/rcw/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX com: <https://cours.iut-orsay.fr/commune>
@@ -10,7 +9,6 @@ SELECT (?score AS ?scoreVilleFleurie)
        (ROUND(AVG(xsd:decimal(?no2)) * 10) / 10 AS ?moyenneNO2)
        (ROUND(AVG(xsd:decimal(?o3)) * 10) / 10 AS ?moyenneO3)
        (ROUND(AVG(xsd:decimal(?pm10)) * 10) / 10 AS ?moyennePM10)
-	   (ROUND(AVG(xsd:decimal(?densite)) * 10) / 10 AS ?moyenneDensite)
 WHERE {
     ?com rdf:type rcw:Commune;
          rcw:scoreFleur ?score;
@@ -18,11 +16,8 @@ WHERE {
          rcw:no2 ?no2 ;
          rcw:o3 ?o3 ;
          rcw:pm10 ?pm10 ;
-         rcw:population2016 ?population2016 ;
-         rcw:surface ?surface .
-    BIND(xsd:decimal(?population2016) / xsd:decimal(?surface) AS ?densite)
 } 
-GROUP BY ?score
+GROUP BY ?score ORDER BY ?scoreVilleFleurie
   `;
   
   const url = endpoint + "?query=" + encodeURIComponent(query);
